e27aaea91e71aad3726160de82a42e004ed4c3cd
chenjenping <chen.jen.ping@hotmail.com>
2011-12-22 22:20:14 +0800

    adjust route and refine some code

--- tripleapp.py
+++ tripleapp.py
@@ -1,58 +1,66 @@
 import cgi
 import os
-from google.appengine.ext.webapp import template
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
 
 class Greeting(db.Model):
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp.RequestHandler):
-  def get(self):
-    greetings_query = Greeting.all().order('-date')
-    greetings = greetings_query.fetch(10)
+    def get(self):
+        template_values = {
+        }
 
-    if users.get_current_user():
-      url = users.create_logout_url(self.request.uri)
-      url_linktext = 'Logout'
-      user = users.get_current_user().nickname()
-    else:
-      url = users.create_login_url(self.request.uri)
-      url_linktext = 'Login'
-      user = 'anonymous'
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
 
-    template_values = {
-      'greetings': greetings,
-      'url': url,
-      'url_linktext': url_linktext,
-      'user': user,
-      }
+class FamilyWall(webapp.RequestHandler):
+    def get(self):
+        greetings_query = Greeting.all().order('-date')
+        greetings = greetings_query.fetch(10)
 
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Log out'
+            user = users.get_current_user().nickname()
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Log in'
+            user = 'anonymous'
+
+        template_values = {
+            'greetings': greetings,
+            'url': url,
+            'url_linktext': url_linktext,
+            'user': user
+        }
+
+        path = os.path.join(os.path.dirname(__file__), 'familywall.html')
+        self.response.out.write(template.render(path, template_values))
 
 class Guestbook(webapp.RequestHandler):
-  def post(self):
-    greeting = Greeting()
+    def post(self):
+        greeting = Greeting()
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+        if users.get_current_user():
+            greeting.author = users.get_current_user()
 
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/')
+        greeting.content = self.request.get('content')
+        greeting.put()
+        self.redirect('/familywall')
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage),
+application = webapp.WSGIApplication([('/', MainPage),
+                                      ('/familywall', FamilyWall),
                                       ('/sign', Guestbook)],
                                      debug=True)
 
 def main():
   run_wsgi_app(application)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main()
